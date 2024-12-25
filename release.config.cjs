8bac48baa85e05fb0cd040401fe81b77959d6c8e
gaurav404 <gaurav.agarwala19@gmail.com>
2024-12-25 19:39:38 +0530

    chore: added auto

---
+++ release.config.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+    branches: [
+      'major',
+      {
+        name: 'main',
+        prerelease: true
+      }
+    ],
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      [
+        '@semantic-release/changelog',
+        {
+          changelogFile: 'CHANGELOG.md'
+        }
+      ],
+      [
+        '@semantic-release/npm',
+      ],
+      '@semantic-release/github',
+      [
+        '@semantic-release/git',
+        {
+          assets: ['CHANGELOG.md'],
+          message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+        }
+      ]
+    ]
+  }
+  
