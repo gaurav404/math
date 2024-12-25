404fbafa096890ecc9735435fc0c132ac81c9082
gaurav404 <gaurav.agarwala19@gmail.com>
2024-12-25 20:48:30 +0530

    feat: added new export

--- src/eveningGreet.ts
+++ src/eveningGreet.ts
@@ -1,3 +1,6 @@
 export const eveningGreet = (name: string) => {
     return `Good morning, ${name}!`;
 }
+export const afternoonGreet = (name: string) => {
+    return `Good afternoon, ${name}!`;
+}
